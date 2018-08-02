d65afd5696709993480e7d1695142bd0d5e66921
Karim Aouadi <kdolphinus@gmail.com>
2018-08-02 23:58:09 +0200

    updated tutorial-1

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+
+<div class="navbar">
+
+    <nav class="deep-purple darken-1">
+
+        <div class="container">
+
+            <a href="" class="brand-logo">GeoNinjas</a>
+            <ul class="right"></ul>
+            <li><a href=""></a>Signup</li>
+            <li><a href=""></a>Login</li>
+
+        </div>
+
+    </nav>
+
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+            
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
