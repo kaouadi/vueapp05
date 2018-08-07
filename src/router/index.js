81ae7053ecf6018c5f3a51f6b916375aa06a1c02
Karim Aouadi <kdolphinus@gmail.com>
2018-08-07 17:06:32 +0200

    Updated tutorial 11

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,20 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from "@/components/auth/Signup";
 import Login from "@/components/auth/Login";
+import firebase from 'firebase';
 
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta:{
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -26,3 +30,21 @@ export default new Router({
     }
 ]
 })
+
+router.beforeEach((to, from, next) => {
+  // Check to see if route reqire auth
+  if(to.matched.some(rec => rec.meta.requiresAuth)){
+    let user = firebase.auth().currentUser
+    if(user){
+      //user signed
+      next()
+    }else{
+      next({name: 'Login'})
+    }
+  }else
+  {
+    next()
+  }
+})
+
+export default router
